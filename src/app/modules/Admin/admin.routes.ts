3f16014c442af6cd3b3e9e7dd2beb13a5ec7816c
mahabube <info.freelancermahabub@gmail.com>
2024-03-22 19:57:43 +0600

    added zod validations

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,27 +1,13 @@
 import express, {NextFunction, Request, Response} from "express";
 import {AdminController} from "./admin.controllers";
-import {AnyZodObject, z} from "zod";
+import validateRequest from "../../middlewares/validateRequest";
+import {adminValidationSchemas} from "./admin.validation";
+
 const router = express.Router();
 
-const update = z.object({
-  body: z.object({
-    name: z.string().optional(),
-    contactNumber: z.string().optional(),
-  }),
-});
-const validateRequest =
-  (schema: AnyZodObject) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({body: req.body});
-      return next();
-    } catch (error) {
-      next(error);
-    }
-  };
-router.get("/", validateRequest(update), AdminController.getAllFromDB);
+router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getByIdFromDB);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(adminValidationSchemas.update), AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDB);
 router.delete("/soft/:id", AdminController.softDeleteFromDB);
 export const adminRoutes = router;
--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -2,8 +2,10 @@ import {Admin, Prisma, UserStatus} from "@prisma/client";
 import {adminSearchAbleFields} from "./admin.constant";
 import {paginationHelper} from "../../../helpars/paginationHelper";
 import prisma from "../../../shared/prisma";
+import { IAdminFilterRequest } from "./admin.interface";
+import { IPaginationOptions } from "../../interfaces/pagination";
 
-const getAllFromDB = async (params: any, options: any) => {
+const getAllFromDB = async (params: IAdminFilterRequest, options: IPaginationOptions) => {
   const {limit, page, skip} = paginationHelper.calculatePagination(options);
   const {searchTerm, ...filterData} = params;
 
@@ -13,7 +15,7 @@ const getAllFromDB = async (params: any, options: any) => {
     andConditions.push({
       AND: Object.keys(filterData).map((key) => ({
         [key]: {
-          equsls: filterData[key],
+          equsls: (filterData as any)[key],
         },
       })),
     });
