99f3d109b96571cdd191aeacc7903879d2a3d55e
mahabube <info.freelancermahabub@gmail.com>
2024-04-15 18:52:41 +0600

    added doctorschedule

--- src/app/modules/Schedule/schedule.controller.ts
+++ src/app/modules/Schedule/schedule.controller.ts
@@ -3,6 +3,9 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import { ScheduleService } from "./schedule.sevice";
+import { Request, Response } from "express";
+import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
 
 const inserIntoDB = catchAsync(async (req, res) => {
     const result = await ScheduleService.inserIntoDB(req.body);
@@ -15,7 +18,23 @@ const inserIntoDB = catchAsync(async (req, res) => {
     });
 });
 
+const getAllFromDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const filters = pick(req.query, ['startDate', 'endDate']);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+    const user = req.user;
+    const result = await ScheduleService.getAllFromDB(filters, options, user as IAuthUser);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Schedule fetched successfully!",
+        data: result
+    });
+});
+
 
 export const ScheduleController = {
-    inserIntoDB
+    inserIntoDB,
+    getAllFromDB
 };
--- src/app/modules/Schedule/schedule.sevice.ts
+++ src/app/modules/Schedule/schedule.sevice.ts
@@ -1,9 +1,12 @@
 import {addHours, addMinutes, format} from "date-fns";
 import prisma from "../../../shared/prisma";
-import { ISchedule } from "./schedule.interface";
-import { Schedule } from "@prisma/client";
+import {IFilterRequest, ISchedule} from "./schedule.interface";
+import {Prisma, Schedule} from "@prisma/client";
+import { paginationHelper } from "../../../helpars/paginationHelper";
+import { IAuthUser } from "../../interfaces/common";
+import { IPaginationOptions } from "../../interfaces/pagination";
 
-const inserIntoDB = async (payload: ISchedule) : Promise<Schedule[]> => {
+const inserIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
   const {startDate, endDate, startTime, endTime} = payload;
 
   const interverlTime = 30;
@@ -64,6 +67,96 @@ const inserIntoDB = async (payload: ISchedule) : Promise<Schedule[]> => {
   return schedules;
 };
 
+const getAllFromDB = async (
+  filters: IFilterRequest,
+  options: IPaginationOptions,
+  user: IAuthUser
+) => {
+  const {limit, page, skip} = paginationHelper.calculatePagination(options);
+  const {startDate, endDate, ...filterData} = filters;
+
+  const andConditions = [];
+
+  if (startDate && endDate) {
+    andConditions.push({
+      AND: [
+        {
+          startDateTime: {
+            gte: startDate,
+          },
+        },
+        {
+          endDateTime: {
+            lte: endDate,
+          },
+        },
+      ],
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => {
+        return {
+          [key]: {
+            equals: (filterData as any)[key],
+          },
+        };
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.ScheduleWhereInput =
+    andConditions.length > 0 ? {AND: andConditions} : {};
+
+  const doctorSchedules = await prisma.doctorSchedules.findMany({
+    where: {
+      doctor: {
+        email: user?.email,
+      },
+    },
+  });
+
+  const doctorScheduleIds = doctorSchedules.map(
+    (schedule) => schedule.scheduleId
+  );
+  
+
+  const result = await prisma.schedule.findMany({
+    where: {
+      ...whereConditions,
+      id: {
+        notIn: doctorScheduleIds,
+      },
+    },
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {[options.sortBy]: options.sortOrder}
+        : {
+            createdAt: "desc",
+          },
+  });
+  const total = await prisma.schedule.count({
+    where: {
+      ...whereConditions,
+      id: {
+        notIn: doctorScheduleIds,
+      },
+    },
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 export const ScheduleService = {
   inserIntoDB,
+  getAllFromDB,
 };
